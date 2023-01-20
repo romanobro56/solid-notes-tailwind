2f2dfeb7203a2c92490ed0fb6a9aa602cb59d042
romanobro56 <romanp2929@gmail.com>
2023-01-20 09:26:52 -0500

    tailwind loox good

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, batch, For } from "solid-js";
+import { render } from "solid-js/web";
+
+type TodoItem = { title: string; done: boolean };
 
 const App: Component = () => {
+  const [newTitle, setTitle] = createSignal("");
+  const [todos, setTodos] = createLocalStore<TodoItem[]>("todos", []);
+
   return (
-    <div class="container mt-5 text-center">
-    <h1 class="mb-4">Whattodo!</h1>
-
-    <form class="mb-5 row row-cols-2 justify-content-center">
-      <input type="text" class="input-group-text p-1 w-25" placeholder="Add task here..." id="taskInput" required />
-
-      <button class="btn btn-primary ms-3 w-auto" type="submit">
-        Add task
-      </button>
-    </form>
-
-    <div>
-      <h4 class="text-muted mb-4">Tasks</h4>
-      <div class="row row-cols-3 mb-3 justify-content-center">
-        <button class="btn btn-danger w-auto">X</button>
-        <div class="bg-light p-2 mx-2">Push code to GitHub</div>
-        <input type="checkbox" role="button" class="form-check-input h-auto px-3" />
+    <div class="flex flex-col justify-center">
+      <h1 class="mb-4 self-center text-zinc-800 hover:text-sky-800 text-4xl mt-4">note app</h1>
+      <div class="h-screen flex flex-col items-center">
+        <form class="bg-gray-600 shadow-lg rounded w-8/12 flex flex-col">
+          <input type="text" id="large-input" class="block w-11/12 p-4 self-center text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-5" />
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Add task
+          </button>
+        </form>
       </div>
     </div>
-  </div>
   )
 }
 
